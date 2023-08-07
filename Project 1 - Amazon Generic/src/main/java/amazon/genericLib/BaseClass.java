3987dce3690b1b636d8369b0d80c5d9540fc16b0
Tilak <tilakakumarp@gmail.com>
2023-08-07 18:45:16 +0530

    renamed project

--- Project 1 - Amazon Generic/src/main/java/amazon/genericLib/BaseClass.java
+++ Project 1 - Amazon Generic/src/main/java/amazon/genericLib/BaseClass.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -11,28 +12,28 @@ import org.testng.annotations.BeforeMethod;
 
 public class BaseClass {
 
-		public WebDriver driver;
-		@BeforeClass
-		public void launchBrowser() {
-			System.setProperty("webdriver.chrome.driver","C:\\Users\\91740\\OneDrive\\Desktop\\Projects\\chromedriver_win32\\chromedriver.exe");
-			driver=new ChromeDriver();
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		}
-		@BeforeMethod
-		public void launchHomePage() {
-			driver.get("https://www.amazon.in/");	
-			}
-		
-		@AfterMethod
-		public void logout() {
-				
-			}
-		
-		@AfterClass
-		public void closeBrowser() {
-			driver.quit();
-		}
+	public WebDriver driver;
 
+	@BeforeClass
+	public void launchBrowser() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 
+	@BeforeMethod
+	public void launchHomePage() {
+		driver.get("https://www.amazon.in/");
+	}
+
+	@AfterMethod
+	public void logout() {
+		Reporter.log("Logout from web Application Successful", true);
+	}
+
+	@AfterClass
+	public void closeBrowser() {
+		driver.close();
+	}
+
+}
